a0f808df380c3d4d75e33ac3b8bdc23f6a5827ee
Edward <eduardo__lps@hotmail.com>
2020-09-30 10:15:31 +0100

    Add boxplot

--- data_understanding.py
+++ data_understanding.py
@@ -3,14 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 with open('./files/loan_train.csv') as csv_file:
-    attrs = {'loan_id': [], 'amount': [], 'duration': []}
+    attrs = {'amount': [], 'duration': []}
     #payments = amount / duration => Correlated, discard
     csv_reader = csv.reader(csv_file, delimiter=';')
     next(csv_reader)
 
     for row in csv_reader:
         if len(row) == 7:
-            attrs['loan_id'].append(int(row[0]))
             attrs['amount'].append(int(row[3]))
             attrs['duration'].append(int(row[4]))
         else:
@@ -18,8 +17,14 @@ with open('./files/loan_train.csv') as csv_file:
 
     for attr in attrs.keys():
         attr_array = attrs[attr]
+        r = plt.boxplot(attr_array, vert=False)
+
         print(attr, 'Max: ' + str(max(attr_array)), 'Min: ' + str(min(attr_array)), 
-            'Avg: ' + str(sum(attr_array) // len(attr_array)), sep=' | ')
+            'Avg: ' + str(sum(attr_array) // len(attr_array)), 'Min.Thresh: ' +  str(r['whiskers'][0].get_xdata()[1]), 
+            'Max.Thresh ' + str(r['whiskers'][1].get_xdata()[1]), sep=' | ')
+        plt.title('Loan Amounts - ' + attr)
+        plt.show()
+        plt.savefig('loan_' + attr + '_box.png')
     '''
     plt.scatter(ids, amounts, s=1, alpha=0.5)
     plt.title('Loan Amounts - Scatter')
