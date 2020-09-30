e18ecf4e178ad6444f4d1179fef72f7123d5a2f8
Edward <eduardo__lps@hotmail.com>
2020-09-30 21:45:20 +0100

    Add extra fields to clean data table

--- data_preparation.py
+++ data_preparation.py
@@ -11,10 +11,12 @@ def clean_data():
 
         for row in csv_reader:
             if len(row) == 7:
+                id = int(row[0])
                 amount = int(row[3])
                 duration = int(row[4])
+                status = int(row[6])
                 if(amount >= attr_data['amount'][0] and amount <= attr_data['amount'][1]
                     and duration >= attr_data['duration'][0] and duration <= attr_data['duration'][1]):
-                    csv_writer.writerow([row[3], row[4]])
+                    csv_writer.writerow([row[0], row[3], row[4], row[6]])
 
 clean_data()
