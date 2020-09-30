eedd4a77ba55bd14c4b625f44c0291708938bc65
Edward <eduardo__lps@hotmail.com>
2020-09-30 15:24:09 +0100

    Add outlier removal

---
+++ data_preparation.py
@@ -0,0 +1,20 @@
+
+import csv
+import data_understanding
+
+def clean_data():
+    with open('./files/loan_train.csv') as dirty_file, open('./files/loan_train_clean.csv', 'w', newline='') as clean_file:
+        csv_reader = csv.reader(dirty_file, delimiter=';')
+        csv_writer = csv.writer(clean_file, delimiter=';')
+        attr_data = data_understanding.analyse_data()
+        next(csv_reader)
+
+        for row in csv_reader:
+            if len(row) == 7:
+                amount = int(row[3])
+                duration = int(row[4])
+                if(amount >= attr_data['amount'][0] and amount <= attr_data['amount'][1]
+                    and duration >= attr_data['duration'][0] and duration <= attr_data['duration'][1]):
+                    csv_writer.writerow([row[3], row[4]])
+
+clean_data()
--- data_understanding.py
+++ data_understanding.py
@@ -1,36 +1,37 @@
 import csv
-import numpy as np
 import matplotlib.pyplot as plt
 
-with open('./files/loan_train.csv') as csv_file:
-    attrs = {'amount': [], 'duration': []}
-    #payments = amount / duration => Correlated, discard
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    next(csv_reader)
+def analyse_data():
+    with open('./files/loan_train.csv') as csv_file:
+        attrs = {'amount': [], 'duration': []}
+        attr_data = {}
+        #payments = amount / duration => Correlated, discard
+        csv_reader = csv.reader(csv_file, delimiter=';')
+        next(csv_reader)
 
-    for row in csv_reader:
-        if len(row) == 7:
-            attrs['amount'].append(int(row[3]))
-            attrs['duration'].append(int(row[4]))
-        else:
-            print('Loan ID ' + row[0] + ' has missing values')
+        for row in csv_reader:
+            if len(row) == 7:
+                attrs['amount'].append(int(row[3]))
+                attrs['duration'].append(int(row[4]))
+            else:
+                print('Loan ID ' + row[0] + ' has missing values')
 
-    for attr in attrs.keys():
-        attr_array = attrs[attr]
-        r = plt.boxplot(attr_array, vert=False)
+        for attr in attrs.keys():
+            attr_array = attrs[attr]
+            r = plt.boxplot(attr_array, vert=False)
+            minThresh = r['whiskers'][0].get_xdata()[1]
+            maxThresh = r['whiskers'][1].get_xdata()[1]
 
-        print(attr, 'Max: ' + str(max(attr_array)), 'Min: ' + str(min(attr_array)), 
-            'Avg: ' + str(sum(attr_array) // len(attr_array)), 'Min.Thresh: ' +  str(r['whiskers'][0].get_xdata()[1]), 
-            'Max.Thresh ' + str(r['whiskers'][1].get_xdata()[1]), sep=' | ')
-        plt.title('Loan Amounts - ' + attr)
-        plt.show()
-        plt.savefig('loan_' + attr + '_box.png')
-    '''
-    plt.scatter(ids, amounts, s=1, alpha=0.5)
-    plt.title('Loan Amounts - Scatter')
-    plt.xlabel('Loan ID')
-    plt.ylabel('Amount')
+            attr_data[attr] = (minThresh, maxThresh)
 
-    plt.show()
-    plt.savefig('loan_amounts_scatter.png')
-    '''
+            print(attr, 'Max: ' + str(max(attr_array)), 'Min: ' + str(min(attr_array)), 
+                'Avg: ' + str(sum(attr_array) // len(attr_array)), 'Min.Thresh: ' +  str(attr_data[attr][0]), 
+                'Max.Thresh ' + str(attr_data[attr][1]), sep=' | ')
+            
+            plt.title('Loan Amounts - ' + attr)
+            #plt.show()
+            plt.savefig('loan_' + attr + '_box.png')
+            
+        return attr_data
+
+analyse_data()
