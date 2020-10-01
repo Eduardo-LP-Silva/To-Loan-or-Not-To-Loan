abead008fe54cd3797b99911c81d6a125a5b36ff
Eduardo Silva <eduardo__lps@hotmail.com>
2020-10-02 00:53:20 +0100

    Add further data analysis

--- data_understanding.py
+++ data_understanding.py
@@ -2,13 +2,83 @@ import csv
 import matplotlib.pyplot as plt
 
 attr_data = {'loan_status_appr': 0, 'loan_status_rej': 0, 'missing_loans': 0, 'missing_districts': 0, 
-    'missing_dispositions': 0, 'missing_cards': 0, 'missing_accounts': 0}
+    'missing_dispositions': 0, 'missing_cards': 0, 'missing_accounts': 0, 'frequency_monthly': 0, 
+    'frequency_transactional': 0, 'cards_classic': 0, 'cards_junior': 0, 'cards_gold': 0, 'disposition_owner': 0,
+    'disposition_disponent': 0}
 
 def analyse_data():
     calc_missing_values()
     analyse_loans()
+    analyse_accounts()
+    analyse_cards()
+    analyse_dispositions()
+    analyse_districts()
     return attr_data
 
+def analyse_districts():
+    with open('./files/district.csv') as districts:
+        attrs = {'inh': [], 'mun_lt_499': [], 'mun_500_1999': [], 'mun_2000_9999': [], 'mun_gt_10000': [],
+            'cities': [], 'urban_inh_r': [], 'avg_salary': [], 'unemp_96': [], 'enterp_p_1000': [], 'crimes_96': []}
+        dist_reader = csv.reader(districts, delimiter=';')
+        next(dist_reader)
+
+        for row in dist_reader:
+            if len(row) == 16:
+                attrs['inh'].append(int(row[3]))
+                attrs['mun_lt_499'].append(int(row[4]))
+                attrs['mun_500_1999'].append(int(row[5]))
+                attrs['mun_2000_9999'].append(int(row[6]))
+                attrs['mun_gt_10000'].append(int(row[7]))
+                attrs['cities'].append(int(row[8]))
+                attrs['urban_inh_r'].append(float(row[9]))
+                attrs['avg_salary'].append(int(row[10]))
+                attrs['unemp_96'].append(float(row[12]))
+                attrs['enterp_p_1000'].append(int(row[13]))
+                attrs['crimes_96'].append(int(row[15]))
+
+        plot_box(attrs, 'Districts')
+
+def analyse_dispositions():
+    with open('./files/disp.csv') as dispositions:
+        disp_reader = csv.reader(dispositions, delimiter=';')
+        next(disp_reader)
+
+        for disp in disp_reader:
+            if len(disp) == 4:
+                attr_data['disposition_' + disp[3].lower()] += 1
+
+        plot_pie([attr_data['disposition_owner'], attr_data['disposition_disponent']], ['Owner', 'Disponent'], 
+            'Disposition')
+
+def analyse_cards():
+    with open('./files/card_train.csv') as cards:
+        card_reader = csv.reader(cards, delimiter=';')
+        next(card_reader)
+
+        for card in card_reader:
+            if len(card) == 4:
+                attr_data['cards_' + card[2]] += 1
+
+        plot_pie([attr_data['cards_classic'], attr_data['cards_junior'], attr_data['cards_gold']], ['Classic', 'Junior', 
+            'Gold'], 'Card Type')
+
+
+def analyse_accounts():
+    with open('./files/account.csv') as accounts:
+        acc_reader = csv.reader(accounts, delimiter=';')
+        next(acc_reader)
+
+        for account in acc_reader:
+            if len(account) == 4:
+                if account[2] == 'monthly issuance':   
+                    attr_data['frequency_monthly'] += 1
+                else:
+                    attr_data['frequency_transactional'] += 1
+
+        plot_pie([attr_data['frequency_monthly'], attr_data['frequency_transactional']], ['Monthly', 'After Transaction'], 
+            'Account Issuance Frequency')
+
+        
 def analyse_loans():
     with open('./files/loan_train.csv') as loans:
         attrs = {'amount': [], 'duration': [], 'payments': []}
@@ -16,30 +86,17 @@ def analyse_loans():
         next(loans_reader)
 
         for row in loans_reader:
-            attrs['amount'].append(int(row[3]))
-            attrs['duration'].append(int(row[4]))
-            attrs['payments'].append(int(row[5]))
+            if len(row) == 7:
+                attrs['amount'].append(int(row[3]))
+                attrs['duration'].append(int(row[4]))
+                attrs['payments'].append(int(row[5]))
 
-        draw_loan_pie_chart()
+        plot_pie([attr_data['loan_status_appr'], attr_data['loan_status_rej']], ['approved', 'rejected'], 'Loan Status')
 
         for key in attr_data.keys():
             print(key, str(attr_data[key]), sep=': ')
 
-        for attr in attrs.keys():
-            attr_array = attrs[attr]
-            r = plt.boxplot(attr_array, vert=False)
-            minThresh = r['whiskers'][0].get_xdata()[1]
-            maxThresh = r['whiskers'][1].get_xdata()[1]
-
-            attr_data[attr] = (minThresh, maxThresh)
-
-            print(attr, 'Max: ' + str(max(attr_array)), 'Min: ' + str(min(attr_array)), 
-                'Avg: ' + str(sum(attr_array) // len(attr_array)), 'Min.Thresh: ' +  str(attr_data[attr][0]), 
-                'Max.Thresh ' + str(attr_data[attr][1]), sep=' | ')
-            
-            plt.title('Loan - ' + attr)
-            #plt.show()
-            plt.savefig('loan_' + attr + '_box.png')
+        plot_box(attrs, 'Loans')
 
 def calc_missing_values():
     with open('./files/loan_train.csv') as loans, open('./files/account.csv') as accounts, open('./files/card_train.csv') as cards, open('./files/disp.csv') as dispositions, open('./files/district.csv') as districts:
@@ -105,17 +162,31 @@ def calc_missing_values():
             if missing_vals[key]:
                 attr_data[key] += 1
 
-def draw_loan_pie_chart():
-    total_loans = attr_data['loan_status_appr'] + attr_data['loan_status_rej']
-    loan_labels = ['approved', 'rejected']
-    loan_sizes = [(attr_data['loan_status_appr'] / total_loans) * 100, 
-        (attr_data['loan_status_rej'] / total_loans) * 100]
+def plot_box(attrs, title):
+    for attr in attrs.keys():
+        attr_array = attrs[attr]
+        r = plt.boxplot(attr_array, vert=False)
+        minThresh = r['whiskers'][0].get_xdata()[1]
+        maxThresh = r['whiskers'][1].get_xdata()[1]
 
+        attr_data[attr] = (minThresh, maxThresh)
+
+        print(attr, 'Max: ' + str(max(attr_array)), 'Min: ' + str(min(attr_array)), 
+            'Avg: ' + str(sum(attr_array) // len(attr_array)), 'Min.Thresh: ' +  str(attr_data[attr][0]), 
+            'Max.Thresh ' + str(attr_data[attr][1]), sep=' | ')
+        
+        plt.title(title + ' - ' + attr)
+        #plt.show()
+        plt.savefig('./figures/' + title + '_' + attr + '_box.png')
+        plt.close()
+
+def plot_pie(sizes, labels, title):
     _, loan_chart = plt.subplots()
-    loan_chart.pie(loan_sizes, labels=loan_labels, autopct='%1.1f%%', startangle=90)
+    loan_chart.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
     loan_chart.axis('equal')
     #plt.show()
-    plt.title('Loan Status')
-    plt.savefig('loan_status.png')
+    plt.title(title)
+    plt.savefig('./figures/' +  title + '.png')
+    plt.close()
 
 analyse_data()
